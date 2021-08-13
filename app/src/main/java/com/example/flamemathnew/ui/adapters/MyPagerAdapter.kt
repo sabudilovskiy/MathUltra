ffa056355b20b1a45d44642f28cd57511d121102
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2021-08-13 11:50:34 +0300

    Practice/theory viewpager

---
+++ app/src/main/java/com/example/flamemathnew/ui/adapters/MyPagerAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.flamemathnew.ui.adapters
+
+import android.content.Context
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.flamemathnew.ui.algebra.PracticeAlgebraFragment
+
+class MyPagerAdapter(private val context: Context, manager: FragmentManager?, val key : String)
+    : FragmentPagerAdapter(manager!!, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        var type = "Практика"
+        when (position) {
+            0 -> type = "Практика"
+            1 -> type = "Теория"
+        }
+
+        when(key){
+            "Linal"->{
+                if(type == "Практика"){
+                    return PracticeAlgebraFragment()
+                }
+            }
+        }
+        return ActionFragment.newInstance(type)
+    }
+
+
+    override fun getPageTitle(position: Int): CharSequence {
+        when (position) {
+            0 -> return "Практика"
+            1 -> return "Теория"
+        }
+        Log.e("PagerAdapter getItem", "Error at position$position")
+        return "Практика"
+
+    }
+}
--- app/src/main/java/com/example/flamemathnew/ui/algebra/AlgebraFragment.kt
+++ app/src/main/java/com/example/flamemathnew/ui/algebra/AlgebraFragment.kt
@@ -1,16 +1,29 @@
 package com.example.flamemathnew.ui.algebra
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.example.flamemathnew.R
 import com.example.flamemathnew.databinding.FragmentAlgebraBinding
+import com.example.flamemathnew.ui.adapters.MyPagerAdapter
+import com.google.android.material.tabs.TabLayout
 
 class AlgebraFragment : Fragment() {
+    private var myPagerAdapter: MyPagerAdapter? = null
+    private lateinit var viewPager: ViewPager
+    private lateinit var tabLayout: TabLayout
 
     private var _binding: FragmentAlgebraBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,8 +32,15 @@ class AlgebraFragment : Fragment() {
     ): View {
         _binding = FragmentAlgebraBinding.inflate(inflater, container, false)
 
+        myPagerAdapter = MyPagerAdapter(requireContext(), requireActivity().supportFragmentManager, "linal")
+        tabLayout = binding.tabs
 
-        return  binding.root
+        viewPager = binding.viewPagerAlgebra
+        viewPager.setOffscreenPageLimit(3)
+        viewPager.setAdapter(myPagerAdapter)
+        tabLayout.setupWithViewPager(viewPager)
+
+        return binding.root
     }
 
     override fun onDestroyView() {
